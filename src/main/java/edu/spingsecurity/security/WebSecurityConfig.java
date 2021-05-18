dc3c06d5b1033fad7813e59a7c64beb3f2a3443d
u19086383 <DANLitvinov@sberbank.ru>
2021-05-18 13:33:17 +0300

    init basic

--- src/main/java/edu/spingsecurity/security/WebSecurityConfig.java
+++ src/main/java/edu/spingsecurity/security/WebSecurityConfig.java
@@ -1,14 +1,23 @@
 package edu.spingsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Override
     public void configure(WebSecurity web) {
@@ -22,9 +31,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .authorizeRequests()
             .antMatchers("/company/**", "/user/**").authenticated()
-            .antMatchers("/info").permitAll()
+            .antMatchers("/info","/error").permitAll()
             .antMatchers("/**").denyAll()
             .and()
-            .httpBasic();
+            .httpBasic()
+            .realmName("Realm");
+    }
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
